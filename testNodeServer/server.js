8b058f1fa45d2171d7d78ba3ca59f1a8b1db14b1
Marco Macca <marco.macca@tecnicosuperiorekennedy.it>
2019-09-18 18:49:05 +0200

    add pg connection and fastify.post for registration form

--- testNodeServer/server.js
+++ testNodeServer/server.js
@@ -1,11 +1,22 @@
 const Influx = require('influx');
+const pg = require('pg');
+const connectionStr = {
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'vale46',
+  port: 5434
+};
+const fastifyport = 4000;
+const fastifyip = '192.168.1.5';
 const fastify = require('fastify')({
   logger: true,
-  ignoreTrailingSlash: true  //cercare a cosa serve e se è ancora necessario
+  ignoreTrailingSlash: true
 });
+fastify.register(require('fastify-cors'), {
+  //put options here
+})
 const os = require('os');
-const fastifyport = 4000;
-const fastifyip = '192.168.1.35';
 var contatore = 0;
 const influx = new Influx.InfluxDB({
   host: 'localhost',
@@ -40,7 +51,7 @@ influx.getDatabaseNames()
   })
 fastify.post('/', async (request, reply) => {
   dati = request.body[0];
-  influx.writePoints([
+  await influx.writePoints([
     {
       measurement: 'response_times',
       tags: { host: os.hostname() },
@@ -56,33 +67,50 @@ fastify.post('/', async (request, reply) => {
       },
     }
   ]).catch(err => {
+    reply.status(500)
     console.error(`Error saving data to InfluxDB! ${err.stack}`)
-  })
-  reply.send('Ok');
-  console.log("// " + (++contatore));
-  console.log(request.body);
-});
+  }).then(() =>
+  console.log("// " + (++contatore)),
+  console.log(request.body))
+  reply.status(204)
+ });
 
 //parte in get
-fastify.get('/get',async (request, reply) => {
- await influx.query(`
+fastify.get('/get', async (request, reply) => {
+  await influx.query(`
     select * from response_times `)
     .then(result => {
-      reply.code(200)
-      reply.send(JSON.stringify(result))
+      reply.status(200).send(JSON.stringify(result))
     }).catch(err => {
       reply.status(500).send(err.stack)
     })
 })
+//////////////////////////////////////////////////////// parte per sito web che gestite le richieste di login e registrazione
+fastify.post('/prova', async (request, reply) => {
+  const client = new pg.Client(connectionStr);
+  var dati = request.body;
+  await client.connect()
+    .then(() => console.log('client has connect'));
+  await client.query(`INSERT INTO utenti (username, Email, FullName, Password) VALUES('${dati.UserName}','${dati.Email}','${dati.FullName}','${dati.Password}')`)
+    .then(() => {
+      client.end()
+      console.log('client close without error')
+      reply.status(200).send({ succeded: true });
+    }).catch(err => {
+      client.end()
+      console.log('client close with errors: ' + err)
+      reply.status(500).send(err)
+    })
+});
 
 // Run the server!
 const start = async () => {
   try {
-      await fastify.listen(fastifyport, fastifyip)
-      fastify.log.info(`server listening on ${fastify.server.address().port}`)
+    await fastify.listen(fastifyport, fastifyip)
+    fastify.log.info(`server listening on ${fastify.server.address().port}`)
   } catch (err) {
-      fastify.log.error(err)
-      process.exit(1)
+    fastify.log.error(err)
+    process.exit(1)
   }
 }
 start();
