e96d79990636363062df1118492fc73a8936ea1c
Marco <marco.macca@tecnicosuperiorekennedy.it>
2019-09-26 17:33:37 +0200

    fix local psql password

--- one.sh
+++ one.sh
@@ -4,10 +4,9 @@ echo "avvio di influx db su docker su porta 8086"
 sudo docker run --rm --name=influxdb -d -p 8086:8086 influxdb
 echo "avvio di postgresql su docker porta 5432 se già utilizzata cambiare porta"
 sudo docker run --rm   --name pg-docker -e POSTGRES_PASSWORD=xxxx -d -p 5432:5432  postgres
-echo "password utente postgres xxxx"
-export PGPASSWORD='xxxx'; 
+echo "password utente postgres xxxx" 
 sleep 5;
-sudo psql -h localhost -p 5432 -U postgres -c"CREATE EXTENSION pgcrypto;CREATE TABLE utenti(
+export PGPASSWORD='xxxx'; psql -p 5432 -U postgres -h localhost -c"CREATE EXTENSION pgcrypto;CREATE TABLE utenti(
 
    ID SERIAL PRIMARY KEY   ,
 
