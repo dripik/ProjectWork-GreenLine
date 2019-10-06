9fcc80e853aa60e95575c0e19ae448717bda7aa0
marcomacca <m.macca91@gmail.com>
2019-10-06 10:42:12 +0200

    minor fix one.sh

--- one.sh
+++ one.sh
@@ -3,10 +3,10 @@
 echo "avvio di influx db su docker su porta 8086"
 sudo docker run --rm --name=influxdb -d -p 8086:8086 influxdb
 echo "avvio di postgresql su docker porta 5432 se già utilizzata cambiare porta"
-sudo docker run --rm   --name pg-docker -e POSTGRES_PASSWORD=xxxx -d -p 5432:5432  postgres
-echo "password utente postgres xxxx" 
+sudo docker run --rm   --name pg-docker -e POSTGRES_PASSWORD=password! -d -p 5432:5432  postgres
+echo "password utente postgres password!" 
 sleep 5;
-export PGPASSWORD='xxxx'; psql -p 5432 -U postgres -h localhost -c"CREATE EXTENSION pgcrypto;CREATE TABLE utenti(
+export PGPASSWORD='password!'; psql -p 5432 -U postgres -h localhost -c"CREATE EXTENSION pgcrypto;CREATE TABLE utenti(
 
    ID SERIAL PRIMARY KEY   ,
 
@@ -19,4 +19,4 @@ export PGPASSWORD='xxxx'; psql -p 5432 -U postgres -h localhost -c"CREATE EXTENS
    Password           TEXT    NOT NULL
 
 );"
-echo "completato con successo "
+echo "Creazione completata!! "
